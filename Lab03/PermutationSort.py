5089e066c1fd0d939c801f89285ae867167c3bcb
ReidDye <40401995+reid23@users.noreply.github.com>
2022-09-29 12:14:04 -0700

    finished

--- Lab03/PermutationSort.py
+++ Lab03/PermutationSort.py
@@ -2,17 +2,14 @@
 Author: Reid Dye
 
 Here is my implementation of Permutation Sort and
-a function to benchmark it. For benchmarking, I'm
-using timeit, since that does things like turning
-off garbge collection and executing all the setup
-before starting the timer to improve accuracy.
+a function to benchmark it.
 '''
 from random import randint, random
 import numpy as np
-from timeit import timeit
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-
+from time import perf_counter_ns as time
+from numpy import format_float_positional as ffp
 
 def permutationSort(arr):
     while True:
@@ -26,11 +23,25 @@ def permutationSort(arr):
             if arr[i]>arr[i+1]: break
         else: #yes i know "bad practice" whatever
             return True
-
 def benchmarkSort(n, t):
+    total=0 #total time
+    for i in range(t):
+        print(n, t) 
+        arr = [random() for _ in range(n)] #make list
+
+        #time it
+        start = time()
+        permutationSort(arr)
+        end = time()
+
+        #add the start
+        total += end-start
+    return (total/t)/1000000 #convert ns to ms
+
+    #old, timeit is bad actually
     return (timeit(
         stmt = "permutationSort(arr)",
-        setup = f"arr = [random() for _ in range({n})]", 
+        setup = f"arr = [random() for _ in range({n})]; print('{n}', '{t}')", 
         globals = globals(),
         number = t
     )/t)*1000 # take the average and convert to ms
@@ -42,6 +53,7 @@ def graphBenchmarks(max_len, iters = 10):
     
     # gather all the data
     data = np.array([benchmarkSort(i, iters) for i in x])
+    print(data)
     
     # make the model
     eval_exp = lambda t, a, b, c: a * np.exp(b * t) + c # define the exponential function
@@ -50,7 +62,7 @@ def graphBenchmarks(max_len, iters = 10):
     
     #plot the things
     plt.scatter(x, data, label = 'True Average Runtime (ms)', color = 'orange') # plot actual results
-    plt.plot(x_fine, fitted, label = r'$%fe^{%fx}+%f$' % (popt[0], popt[1], popt[2])) # plot model
+    plt.plot(x_fine, fitted, label = r'$%se^{%sx}+%s$' % (ffp(popt[0], 10), ffp(popt[1], 10), ffp(popt[2], 10))) # plot model
     
     #legend, labels, then show
     plt.legend()
@@ -61,5 +73,5 @@ def graphBenchmarks(max_len, iters = 10):
 #%%
 
 if __name__=='__main__':
-    graphBenchmarks(8, 100)
+    graphBenchmarks(10, 6)
 # %%
